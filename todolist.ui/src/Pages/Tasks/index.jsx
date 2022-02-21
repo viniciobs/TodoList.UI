ca49f5c55dd4389f8b495aeaef6ed87edd9498f9
viniciobs <vinicio.barreto.santos@gmail.com>
2022-02-21 00:15:27 -0300

    Initiating tasks page

--- todolist.ui/src/Pages/Tasks/index.jsx
+++ todolist.ui/src/Pages/Tasks/index.jsx
@@ -3,11 +3,23 @@ import Template from '../../Pages/Template';
 import LoadingSpinner from '../../Components/LoadingSpinner';
 
 const Tasks = () => {
-    const [isLoading, setIsloading] = useState(true);
+    const [isLoading, setIsloading] = useState(false);
 
     return (
         <Template title="Tasks">
             {isLoading ? <LoadingSpinner coverAllPage={true}/> : ''}
+            <form>
+            <div className="form-row">
+                <div className="form-group col-md-1">
+                    <label htmlFor="status">Status</label>
+                    <select id="status" class="form-control">
+                        <option>All</option>
+                        <option value={true}>Completed</option>
+                        <option value={false}>To do</option>
+                    </select>
+                </div>
+            </div>
+            </form>
         </Template>
     )
 
